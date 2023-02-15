e9415af0544903a71f29c469438a032c27f8bae6
Noman <80441451+nali556@users.noreply.github.com>
2023-02-15 13:19:36 +0500

    filter

--- src/components/ArrayFilter.jsx
+++ src/components/ArrayFilter.jsx
@@ -1,26 +1,8 @@
+import products from "../utils/products";
 const ArrayFilter = () => {
-
-    const items = [
-        {
-            id: 1,
-            name: 'Item 1',
-        },
-        {
-            id: 2,
-            name: 'Item 2',
-        },
-        {
-            id: 3,
-            name: 'Item 3',
-        },
-    ];
-
-  
-
-
     return <ol>
         {
-            items.filter(( {id} ) => id !== 3).map(({id, name}) => <li key={id}>{name}</li>)
+            products.filter(( {onSale} ) => onSale ===true).map(({id, name}) => <li key={id}>{name}</li>)
         }
     </ol>
 }
