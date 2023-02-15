8f08e1fe214953e7fc8b01ff793509394aac5b10
Noman <80441451+nali556@users.noreply.github.com>
2023-02-15 13:16:55 +0500

    sort

--- src/components/ArraySort.jsx
+++ src/components/ArraySort.jsx
@@ -1,36 +1,15 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import products from "../utils/products";
 
 const ArraySort = () => {
 
-    const [numbers, setNumbers] = useState([5,2,7,1,8,4,9,3,6]);
-    const [direction, setDirection] = useState('asc');
+    const priceArr = products.map(products => products.price)
+    priceArr.sort((a,b) => a - b);
 
-    // useEffect listens for changes and fires when a change is made
-
-    useEffect(() => {
-        sortNumbers();
-    }, [])
-
-    const sortNumbers = () => {
-        // take the numbers variable and sort them desc if current direction is asc and vice versa
-        // change direction to desc if it was asc and vice versa
-        // and set that sorted array into the numbers variable(piece of state)
-
-        const sortedNumbers = [...numbers];
-        if(direction === 'asc') {
-            sortedNumbers.sort((a,b) => b - a);
-            setDirection('desc');
-        } else {
-            sortedNumbers.sort((a,b) => a - b);
-            setDirection('asc');
-        }
-        setNumbers(sortedNumbers);
-    }
 
     return <div>
         <h3>Numbers:</h3>
-        <p>{numbers.join(', ')}</p>
-        <button onClick={sortNumbers}>ReSort</button> <small>{direction}</small>
+        <p>{priceArr.join(', ')}</p>
     </div>
 }
 
